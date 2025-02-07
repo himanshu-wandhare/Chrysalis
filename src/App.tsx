6d6e5e57d0dae6fd4c91207d75f03840130ac7f9
dhanashree010804 <himanshuwandhare13@gmail.com>
2025-02-07 14:56:06 +0530

    change router to hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Routes,
     Route,
     useLocation,
 } from "react-router-dom";
 
-// Importing Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-// import HeroSection from "./components/HeroSection";
-// import Timeline from "./components/Timeline"; // Assuming Timeline is a component
 import EventsPage from "./pages/EventsPage";
-// import InvitationCard from "./components/InvitationCard";
 import { ReduxProvider } from "./hooks/isPhone";
 import Members from "./pages/Members";
 import Winners from "./pages/Winners";
